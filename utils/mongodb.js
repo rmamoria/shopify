5607d6b5056803c8a9ca528a9864336ffce70a36
Rahul Mamoria <rmamoria@griddynamics.com>
2024-06-02 13:43:25 +0530

    sell products added

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,20 +1,14 @@
 import { MongoClient } from 'mongodb';
-
-// const uri = "mongodb+srv://rahul903:A54KhWEDYn3MMr82@cluster0.htw6gd9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = process.env.MONGODB_URI;
 if (!uri) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
-
 let cachedClient = null;
-
 export async function connectToDatabase() {
     if (cachedClient) {
         return { client: cachedClient, db: cachedClient.db() };
     }
-
     const client = new MongoClient(uri);
-
     try {
         await client.connect();
         cachedClient = client;
